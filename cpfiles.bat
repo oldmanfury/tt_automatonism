21a4456a6c0137c0909e5409dd30a969d5c8ef41
Gerald Stevens <logo64@gmail.com>
2017-12-02 16:09:35 -0800

    Add files via upload

---
+++ cpfiles.bat
@@ -0,0 +1,7 @@
+copy c:\audio\PdP\AUTOMATONISM_2.1\*.pd \\EUROPI\PiShare\AUTOMATONISM_2.1
+
+copy C:\Audio\PdP\AUTOMATONISM_2.1\patch_editor_abs\statesave\1-clock-ss.txt \\EUROPI\PiShare\AUTOMATONISM_2.1\patch_editor_abs\statesave
+copy C:\Audio\PdP\AUTOMATONISM_2.1\patch_editor_abs\statesave\1-maestro4-ss.txt \\EUROPI\PiShare\AUTOMATONISM_2.1\patch_editor_abs\statesave
+copy C:\Audio\PdP\AUTOMATONISM_2.1\patch_editor_abs\statesave\1-triad-ss.txt \\EUROPI\PiShare\AUTOMATONISM_2.1\patch_editor_abs\statesave
+copy C:\Audio\PdP\AUTOMATONISM_2.1\patch_editor_abs\statesave\1-chorus-ss.txt \\EUROPI\PiShare\AUTOMATONISM_2.1\patch_editor_abs\statesave
+
